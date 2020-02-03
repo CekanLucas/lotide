b25da45bf9269f53078f2533248f68f8e4877fb5
Lucas <cekanlucas@gmail.com>
2020-02-03 19:43:13 +0000

    added definition of tail tail.js

--- tail.js
+++ tail.js
@@ -5,6 +5,9 @@ const assertEqual = function(actual, expected) {
   );
 };
 
+/* The tail is not the last element of the array. The tail is meant to be every element except the head (first element) of the array.
+ */
+
 const tail = arr => {
   return arr.slice(1);
 };
