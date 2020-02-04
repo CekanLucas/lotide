b5c240d9886da32d1e2aaa0b4bc190da5b4f5d20
Lucas <cekanlucas@gmail.com>
2020-02-04 21:46:45 +0000

    implemented linting to assertArraysEqual

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,8 +1,8 @@
 // Implement assertArraysEqual which will take in two arrays and console.log an appropriate message to the console.
 
-// Assert Array Function 
+// Assert Array Function
 const assertArraysEqual = (arr1,arr2) => {
-  // if arrays equivalent return true 
+  // if arrays equivalent return true
   const eqArrays = (arr1,arr2) => {
     let i = 0;
     while (i < arr1.length) {
@@ -17,9 +17,9 @@ const assertArraysEqual = (arr1,arr2) => {
     `${arr1}\t\t ✔️ \t\t ${arr2}` :
     `${arr1}\t\t ✖️ \t\t ${arr2}`
   );
-}
+};
 
-assertArraysEqual( [1, 2, 3], [1, 2, 3] ); // => should PASS
-assertArraysEqual( [1, 2, 3], [3, 2, 1] ); // => should FAIL
-assertArraysEqual( ["1", "2", "3"], ["1", "2", "3"] ); // => should PASS
-assertArraysEqual( ["1", "2", "3"], ["1", "2", 3] ); // => should FAIL
+assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
+assertArraysEqual([1, 2, 3], [3, 2, 1]); // => should FAIL
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // => should PASS
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // => should FAIL
