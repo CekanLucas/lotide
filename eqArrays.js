ac21caf9e633bd8aa17a441c546f83af72dab939
Lucas <cekanlucas@gmail.com>
2020-02-04 21:26:57 +0000

    eslinting edits

--- eqArrays.js
+++ eqArrays.js
@@ -9,13 +9,13 @@ const assertEqual = function(actual, expected) {
 // Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
 
 const eqArrays = (arr1,arr2) => {
- i = 0;
-  while(i<arr1.length){
-  if(arr1[i] !== arr2[i] ) return false;
-  i++;
- }
- return true;
-}
+  let i = 0;
+  while (i < arr1.length) {
+    if (arr1[i] !== arr2[i]) return false;
+    i++;
+  }
+  return true;
+};
 
 assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
 assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => should FAIL
