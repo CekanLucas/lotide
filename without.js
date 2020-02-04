2863b02fd829ef5914c246a0e19b9f43b22b567c
Lucas <cekanlucas@gmail.com>
2020-02-04 23:37:51 +0000

    solved without.js

---
+++ without.js
@@ -0,0 +1,48 @@
+const assertArraysEqual = (arr1,arr2) => {
+  // if arrays equivalent return true
+  const eqArrays = (arr1,arr2) => {
+    let i = 0;
+    while (i < arr1.length) {
+      if (arr1[i] !== arr2[i]) return false;
+      i++;
+    }
+    return true;
+  };
+  
+  // AssertEqual
+  console.log(eqArrays(arr1,arr2) ?
+    `${arr1}\t\t ✔️ \t\t ${arr2}` :
+    `${arr1}\t\t ✖️ \t\t ${arr2}`
+  );
+};
+
+// Implement without which will return a subset of a given array, removing unwanted elements.
+
+const without = (include,exclude) => {
+  return include.filter(el => {
+    return exclude.indexOf(el) === -1;
+  });
+};
+
+assertArraysEqual(
+  without(
+    [1, 2, 3],
+    [1]
+  ),
+  [2, 3]
+);
+assertArraysEqual(
+  without(
+    ["1", "2", "3"],
+    [1, 2, "3"]
+  ),
+  ["1", "2"]
+);
+
+assertArraysEqual(
+  without(
+    [1,"a", "b", "3"],
+    [ "a", "3"]
+  ),
+  [1, "b"]
+);
