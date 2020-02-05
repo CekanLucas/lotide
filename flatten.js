59f258ed68cbdf9cd09245deb368e526bc7cf1d5
Lucas <cekanlucas@gmail.com>
2020-02-05 00:11:06 +0000

    solved flatten and added assertion tests

--- flatten.js
+++ flatten.js
@@ -20,13 +20,18 @@ const assertArraysEqual = (arr1,arr2) => {
 
 const flatten = input => {
  let ans = [];
- const extract = (arr) => {
-  console.log('extraction here')
+ const extract = arr => {
+   arr.forEach(el => {
+     Array.isArray(el) ? extract(el) : ans.push(el);
+   }); // recursively flatten array
  }
- input.forEach(el => {
-   Array.isArray(el) ? extract(el) : ans.push(el);
- });
+ extract(input);
+ return ans;
 }
-console.log(flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6]);
 
-// assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6])
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]),[1, 2, 3, 4, 5, 6])
+
+const words = ["hello", "world", "lighthouse"];
+flatten(["hello", "world", "lighthouse"], ["lighthouse"]); // no need to capture return value for this test case
+// Make sure the original array was not altered by the without function
+assertArraysEqual(words, ["hello", "world", "lighthouse"]);
