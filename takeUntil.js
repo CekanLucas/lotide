f657fca3ef4a1547eac1c8967532c8c96825eaf1
Lucas <cekanlucas@gmail.com>
2020-02-06 18:39:14 +0000

    lodash: takeUntil.js

---
+++ takeUntil.js
@@ -0,0 +1,30 @@
+
+// slice of the array with elements taken from the beginning." It should keep going until the callback/predicate returns a truthy value.
+
+const takeUntil = function(array, callback) {
+  let result = [];
+  for (element of array) {
+    if (callback(element)) {
+      return result;
+    } else {
+      result.push(element);
+    }
+  }
+  return result;
+};
+
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);
+
+/* EXPECTED OUTPUT
+[ 1, 2, 5, 7, 2 ]
+---
+[ 'I\'ve', 'been', 'to', 'Hollywood' ] */
